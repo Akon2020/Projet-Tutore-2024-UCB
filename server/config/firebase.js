5fb75b77ea015cd1a41bba759f4bd9246985c3fb
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-09-07 16:02:27 +0200

    securing some informations

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTyL5D29yAPgA02aVakmMSK2x5OGiVNjc",
-  authDomain: "security-app-f3d00.firebaseapp.com",
-  projectId: "security-app-f3d00",
-  storageBucket: "security-app-f3d00.appspot.com",
-  messagingSenderId: "126784578421",
-  appId: "1:126784578421:web:a9e28264364130f5f5693c"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

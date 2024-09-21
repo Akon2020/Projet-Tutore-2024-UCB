01daf6d50a761f024dd28244dc15f794507167ed
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-09-21 12:41:01 +0200

    first chart builded

--- frontend/src/components/graphique/Graphique.jsx
+++ frontend/src/components/graphique/Graphique.jsx
@@ -15,36 +15,43 @@ const Graphique = () => {
       name: "Page A",
       uv: 4000,
       pv: 2400,
+      amt: 2400,
     },
     {
       name: "Page B",
       uv: 3000,
       pv: 1398,
+      amt: 2210,
     },
     {
       name: "Page C",
       uv: 2000,
       pv: 9800,
+      amt: 2290,
     },
     {
       name: "Page D",
       uv: 2780,
-      pv: 9800,
+      pv: 3908,
+      amt: 2000,
     },
     {
       name: "Page E",
       uv: 1890,
       pv: 4800,
+      amt: 2181,
     },
     {
       name: "Page F",
       uv: 2390,
       pv: 3800,
+      amt: 2500,
     },
     {
       name: "Page G",
       uv: 3490,
       pv: 4300,
+      amt: 2100,
     },
   ];
   return (
@@ -58,30 +65,31 @@ const Graphique = () => {
         >
           <defs>
             <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#dc143c" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#dc143c" stopOpacity={0} />
+              <stop offset="5%" stopColor="#ff000073" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#ff000073" stopOpacity={0} />
             </linearGradient>
             <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#800080" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#800080" stopOpacity={0} />
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
             </linearGradient>
           </defs>
-          <CartesianGrid strokeDasharray={"3 3"} />
           <XAxis dataKey="name" />
           <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
           <Tooltip />
           <Area
             type="monotone"
-            dataKey="pv"
-            stroke="#800080"
-            fill="#80008033"
+            dataKey="uv"
+            stroke="#ff000073"
+            fillOpacity={1}
+            fill="url(#colorUv)"
           />
-
           <Area
             type="monotone"
-            dataKey="uv"
-            stroke="#dc143c"
-            fill="#ff000033"
+            dataKey="pv"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
           />
         </AreaChart>
       </ResponsiveContainer>

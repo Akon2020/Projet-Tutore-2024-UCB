401bb974c29f128d02c49a50fe68b664fe0169dc
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-09-23 16:11:45 +0200

    Connect server to front & configure axios

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,21 +1,37 @@
-// import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from '../../utils/axios';
 import "./login.scss";
 
 const Login = () => {
-  // const [values, setValues] = useState({ email: "", password: "" });
-  // const [error, setError] = useState(null);
-  const connexion = () => {
-    alert("submitted");
+  const [values, setValues] = useState({ email: "", password: "" });
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  axios.defaults.withCredentials = true;
+  const connexion = (event) => {
+    event.preventDefault();
+    axios
+      .post("/auth/adminlogin", values)
+      .then((result) => {
+        if (result.data.loginStatus) {
+          navigate("/home");
+        } else {
+          setError(result.data.Error);
+          setTimeout(() => {
+            setError(null);
+          }, 1700);
+        }
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div className="loginPage">
       <div className="loginForm">
-        {/* {error && (
+        {error && (
           <div className="erreur" role="alert">
             {error}
           </div>
-        )} */}
+        )}
         <div className="info">
           <h2 className="title">Login | Portail admin</h2>
           <p>Connectez-vous à votre compte pour ne rien rater</p>
@@ -29,6 +45,7 @@ const Login = () => {
             autoComplete="off"
             placeholder="Entrez votre adresse mail"
             className="email"
+            onChange={(e) => setValues({ ...values, email: e.target.value })}
           />
           <label htmlFor="password"></label>
           <input
@@ -37,6 +54,7 @@ const Login = () => {
             name="password"
             placeholder="Entrez votre mot de passe"
             className="password"
+            onChange={(e) => setValues({ ...values, password: e.target.value })}
           />
           <button>Connexion</button>
         </form>

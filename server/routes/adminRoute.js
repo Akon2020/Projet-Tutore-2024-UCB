121cf360098495ac8111b7cb85aa1bdfec386630
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-10-03 12:57:55 +0200

    solved query problem

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -4,10 +4,23 @@ import jwt from "jsonwebtoken";
 import bcryptjs from "bcryptjs";
 import path from "path";
 import multer from "multer";
+import { Result } from "express-validator";
 
 const router = express.Router();
 router.get("/totalusers", (req, res) => {
-  const sql = "SELECT COUNT(id) as utilisateur FROM utilisateur";
+  const sql = "SELECT COUNT(id_utilisateur) as utilisateur FROM utilisateur";
+  db.query(sql, (err, result) => {
+    if (err) {
+      return res.json({ Status: false, Error: "Query error" });
+    }
+    return res.json({ Status: true, Result: result });
+  });
+});
+
+
+router.get("/totaladmin", (req, res) => {
+  const sql =
+    "SELECT COUNT(id_utilisateur) as admin FROM utilisateur WHERE type_utilisateur = 'admin'";
   db.query(sql, (err, result) => {
     if (err) {
       return res.json({ Status: false, Error: "Query error" });
@@ -21,4 +34,4 @@ router.get("/logout", (req, res) => {
   return res.json({ Status: true });
 });
 
-export {router as adminRoute}
+export { router as adminRoute };

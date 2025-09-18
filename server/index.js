7abc3ee1c83ce867384c574530fa810514bfb0e1
Isaac Akonkwa <akonkwaushindi@gmail.com>
2025-09-18 14:57:48 +0200

    fix:Cors blocker

--- server/index.js
+++ server/index.js
@@ -16,6 +16,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/images", express.static("public/images/uploads"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(bodyParser.urlencoded({ extended: true, limit: "1024mb" }));
 app.use(bodyParser.json({ limit: "1024mb" }));
 app.use(
@@ -23,15 +32,17 @@ app.use(
     origin: [process.env.URL_ORIGIN],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: [
+      "Access-Control-Allow-Origin",
+      "Access-Control-Allow-Credentials",
+    ],
   })
 );
-app.use(express.json());
-app.use(cookieParser());
-app.use("/images", express.static("public/images/uploads"));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use("/", documentationRoute);
 app.use("/auth", adminAuth);
@@ -41,4 +52,5 @@ app.use("/api", publicationRoute);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Le serveur est lancé au http://localhost:${PORT}/`);
+  console.log(process.env.URL_ORIGIN)
 });

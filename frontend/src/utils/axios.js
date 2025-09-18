73ceb77391c48563b341f162a8bebaa21c10bf38
Isaac Akonkwa <akonkwaushindi@gmail.com>
2025-09-18 15:33:26 +0200

    fix:adding baseURL

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://securityapi.burningheartihs.org",
+  baseURL: "http://securityapi.burningheartihs.org",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",

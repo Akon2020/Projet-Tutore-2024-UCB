3f6b4d847c3d37d261d0d1ecb5369fc9df4946b8
Isaac Akonkwa <akonkwaushindi@gmail.com>
2025-10-02 10:52:29 +0200

    removing timeout

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://securityapi.burningheartihs.org",
   // baseURL: "http://localhost:3000",
-  timeout: 5000,
+  // timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
